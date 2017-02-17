ee41fdbb746806ff7ff08c73a6f9a12f20fa56c3
zhiqiang.wu <zhiqiang.wu@chukou1.com>
2017-02-17 17:32:23 +0800

    allow cross-domain headers

--- server.js
+++ server.js
@@ -99,11 +99,10 @@ class Server {
         // support cross-domain access
         app.all('*', function(req, res, next) {
            res.header("Access-Control-Allow-Origin", "*");
-           res.header("Access-Control-Allow-Headers", "X-Requested-With");
+           res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
            res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
            res.header("Access-Control-Expose-Headers", "X-InlineCount");
            res.header("Content-Type", "application/json;charset=utf-8");
-          
            next();
         });
 
