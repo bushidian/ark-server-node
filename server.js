c28bcf49ced1cdd54c4f92d72628b6e7471e790f
zhiqiang.wu <zhiqiang.wu@chukou1.com>
2017-02-17 16:55:09 +0800

    cross-domain, you will need to add Access-Control-Expose-Headers header on the server.

--- server.js
+++ server.js
@@ -101,7 +101,9 @@ class Server {
            res.header("Access-Control-Allow-Origin", "*");
            res.header("Access-Control-Allow-Headers", "X-Requested-With");
            res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+           res.header("Access-Control-Expose-Headers", "X-InlineCount");
            res.header("Content-Type", "application/json;charset=utf-8");
+          
            next();
         });
 
